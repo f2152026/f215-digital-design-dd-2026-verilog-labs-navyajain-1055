30393b7e949e6012b2f393aa138ac73a47eec8dc
navyajain-1055 <f20240774@pilani.bits-pilani.ac.in>
2026-08-29 20:45:02 +0000

    lab01 task4

--- labs/lab01/task4/rca64.v
+++ labs/lab01/task4/rca64.v
@@ -26,5 +26,29 @@ module rca64(
 );
 
   // TODO: your 64-bit ripple-carry structure goes here.
+  wire [64:0] c;
+
+    // Initial carry
+    assign c[0] = cin;
+
+    // Instantiate 64 full adders
+    genvar i;
+
+    generate
+        for (i = 0; i < 64; i = i + 1) begin : gen_fa
+
+            FA_Gate FA (
+                .a(a[i]),
+                .b(b[i]),
+                .cin(c[i]),
+                .sum(sum[i]),
+                .cout(c[i+1])
+            );
+
+        end
+    endgenerate
+
+    // Final carry-out
+    assign cout = c[64];
 
 endmodule
